653afc6f9b8753e546c329377f878dba874bbca9
Luu Kha <luutkha@gmail.com>
2022-12-01 23:45:49 +0700

    [auto-generate/story-book-file] initial commit

--- app.js
+++ app.js
@@ -10,50 +10,65 @@ const upload = multer()
 var exphbs = require('express-handlebars');
 app.engine('hbs', exphbs({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
-var fs = require('fs');
-const { render } = require('express/lib/response');
-const readXlsxFile = require('read-excel-file/node')
-var XLSX = require("xlsx");
-const { readOneSheet } = require('./helper/excelHelper')
-const { writeFile } = require('./helper/fileHelper')
 var _ = require('lodash');
+const fs = require('fs').promises;
+const path = require('path');
+const { text } = require('express');
 
+console.log('START TESTING');
+//const
+const testFolder = 'D:\\WORK\\WorkOnCompany\\HAS\\Code\\gcr\\libs\\ui\\components\\common\\src';
+const listFoldersNeedFilter = ['molecules', 'atoms']
+
+// get all file path on folder
+async function walk(dir) {
+  let files = await fs.readdir(dir);
+  files = await Promise.all(files.map(async file => {
+    const filePath = path.join(dir, file);
+    const stats = await fs.stat(filePath);
+    if (stats.isDirectory()) return walk(filePath);
+    else if (stats.isFile()) return filePath;
+  }));
+
+  return files.reduce((all, folderContents) => all.concat(folderContents), []);
+}
+
+async function getComponentName(path) {
+  try {
+    const data = await fs.readFile(path, { encoding: 'utf8' });
+    const KEY_WORD_FOR_FILTER = 'export default '
+    const regex = /(?<=export default ).*(?=[;*])/g
+    const finalData = data.split('\r').map(text => text.match(regex)).filter(e => e !== null);
+    return _.flatMapDeep(finalData)[0]
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
+async function main() {
+  const listComponentPath = await walk(testFolder);
+
+  // filter compoents here
+  const finalListComponent = [...listComponentPath].filter(p => (p.includes('.jsx') || p.includes('.tsx')) && listFoldersNeedFilter.filter(f => p.includes(f)).length > 0)
+  for (let index = 0; index < finalListComponent.length; index++) {
+    const component = finalListComponent[index];
+    const componentName = await getComponentName(component);
+    if (!componentName) continue;
+    const rawData = {
+      id: index,
+      componentName: componentName,
+      path: component,
+      storyContent: undefined
+    }
+
+    //TO DO
+    console.log(rawData)
+  }
+
+
+
+}
+main()
 //=====
-
-app.get('/', function (req, res) {
-  var workbook = XLSX.readFile("./MultiLanguageScreens.xlsx");
-  var sheet_name_list = workbook.SheetNames; // SheetNames is an ordered list of the sheets in the workbook
-  data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]); //if you have multiple sheets
-  console.log(sheet_name_list)
-  let listData = []
-  for (var key in sheet_name_list) {
-    const tempList = readOneSheet(workbook, sheet_name_list[key])
-    listData = _.concat(listData, tempList)
-  }
-  listData = listData.filter(e => e.en && e.vi && e.Prefix && e.en.length > 0)
-  console.log(listData.length)
-  const contentForWriteEn = {};
-  const contentForWriteVi = {};
-  for (let index = 0; index < listData.length; index++) {
-    const data = listData[index];
-    // if (data.Id === 1) {
-    //   contentForWrite += "\\" + data["Components | Item"]
-    // }
-    contentForWriteEn[data.Prefix] = data.en
-    contentForWriteVi[data.Prefix] = data.vi
-  }
-  writeFile('gdc-health-en.json', JSON.stringify(contentForWriteEn))
-  writeFile('gdc-health-vi.json', JSON.stringify(contentForWriteVi))
-  res.json(listData)
-})
-app.post('/', upload.fields([]), function (req, res) {
-  console.log(req.body)
-  res.render('home');
-});
-
-app.get('/index', function (req, res) {
-  res.send('Hello, index page here!');
-});
-app.listen(3000, function () {
-  console.log('App listening on port 3000!');
-});
+console.log('END TESTING');
